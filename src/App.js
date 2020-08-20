8e6d2c4bfeadb634377a5e0a8abbf325d71a07ec
Abdullahi Abass <36056652+space-w-alker@users.noreply.github.com>
2020-08-20 20:33:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./tailwind.output.css";
+
+const App = () => (
+  <div className="max-w-md mx-auto flex p-6 bg-blue-100 mt-10 rounded-lg shadow-lg">
+    <div className="ml-6 pt-1">
+      <h1 className="text-2xl text-blue-700 leading-tight">
+        Tailwind and Create React App
+      </h1>
+      <p className="text-base text-gray-700 leading-normal">
+        Building apps together
+      </p>
+    </div>
+  </div>
+);
+
+export default App;
